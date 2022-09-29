1688d430440145e3f33eb952b777751836d28a8d
Omar <o.moreta@gmail.com>
2022-09-29 19:43:15 -0400

    added bread model and show route

--- controllers/bread_controller.js
+++ controllers/bread_controller.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const breads = express.Router();
+const Bread = require("../models/bread");
 
 //Index
 breads.get("/", (req, res) => {
-  res.send("This is the index at /breads");
+  res.send(Bread);
+});
+
+//Show
+breads.get("/:arrayIndex", (req, res) => {
+  res.send(Bread[req.params.arrayIndex]);
 });
 
 module.exports = breads;
