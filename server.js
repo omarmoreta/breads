b4864277170b6a8d9df1f64b8184cb33e50a7da8
Omar <o.moreta@gmail.com>
2022-09-29 19:40:05 -0400

    added breads controller

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to an Awesome App about Breads!");
 });
 
+//Breads
+const breadsController = require("./controllers/bread_controller");
+app.use("/breads", breadsController);
+
 //Listen
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
