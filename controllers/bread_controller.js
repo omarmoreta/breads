99285ba798c0de890d3dd10a91a3b7e72c30d408
Omar <o.moreta@gmail.com>
2022-09-30 23:48:03 -0400

    sneding index and show data

--- controllers/bread_controller.js
+++ controllers/bread_controller.js
@@ -10,7 +10,11 @@ breads.get("/", (req, res) => {
 
 //Show
 breads.get("/:arrayIndex", (req, res) => {
-  res.send(Bread[req.params.arrayIndex]);
+  if (Bread[req.params.arrayIndex]) {
+    res.render("Show", { bread: Bread[req.params.arrayIndex] });
+  } else {
+    res.send("404");
+  }
 });
 
 module.exports = breads;
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
 
 //Routes
 app.get("/", (req, res) => {
@@ -19,6 +20,11 @@ app.get("/", (req, res) => {
 //Breads
 const breadsController = require("./controllers/bread_controller");
 app.use("/breads", breadsController);
+
+// 404 catch-all route
+app.get("*", (req, res) => {
+  res.send("404");
+});
 
 //Listen
 app.listen(PORT, () => {
