f8ffdcd2743483ec5b94c638af1295597e8c7407
Omar <o.moreta@gmail.com>
2022-10-07 23:27:12 -0400

    added default picture

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # breads
+
 Backend and APIs - breadCRUD activity
+
+### Images
+
+Default Photo by [Raul Angel](https://unsplash.com/@raulangel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText) on [Unsplash](https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)
--- models/bread.js
+++ models/bread.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const breadSchema = new Schema({
   name: { type: String, required: true },
   hasGluten: Boolean,
-  image: { type: String, default: "http://placehold.it/500x500.png" },
+  image: { type: String, default: "/images/default.jpg" },
 });
 
 const Bread = mongoose.model("Bread", breadSchema);
