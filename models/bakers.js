6be890bef1bb0c086fbe5c442de7d868a577d5f9
Omar <o.moreta@gmail.com>
2022-10-12 22:15:13 -0400

    added delete baker hook

--- models/bakers.js
+++ models/bakers.js
@@ -23,6 +23,13 @@ bakerSchema.virtual("breads", {
   foreignField: "baker",
 });
 
+//Hooks
+bakerSchema.post("findOneAndDelete", function () {
+  Bread.deleteMany({ baker: this._conditions._id }).then((deleteStatus) => {
+    console.log(deleteStatus);
+  });
+});
+
 //Model
 const Baker = mongoose.model("Baker", bakerSchema);
 
